8f69647cdda9937a73a4368b78daa9e54bf1da91
marcotuliosr <marcotuliopessoal@gmail.com>
2022-09-11 22:17:32 -0300

    Projeto

---
+++ Aula Presencial/2022-08-08/scripts.js
@@ -0,0 +1,6 @@
+nota1 = parseInt(prompt("Informe a primeira nota de 0 a 50:"));
+nota2 = parseInt(prompt("Informe a segunda nota de 0 a 50:"));
+total = (nota1 + nota2);
+alert(total.toFixed(1));
+
+
